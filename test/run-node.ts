b1110bcfd8dfadbe56d3e733ca73b1e24efe1021
AIFanatic <aifanatic@github.com>
2023-09-11 23:56:54 +0000

    Prepare for unification of tests

--- test/run-node.ts
+++ test/run-node.ts
@@ -32,3 +32,51 @@ for(let file of files) {
     console.log(`Running test: ${file}`);
     execSync(`node -r esbuild-runner/register ${file}`, { stdio: 'inherit' });
 }
+
+
+
+
+
+// import { Device } from "../src";
+// import { TestRunner } from "./run-web";
+// import { TensorTests } from "./web/Tensor.test";
+// import { TensorGradTests } from "./web/Tensor.Grad.test";
+// import { NNTests } from "./web/NN.test";
+// import { NetworkTests } from "./web/Network.test";
+// import { NetworksTests } from "./web/networks/Networks.test";
+
+// const UnitTests = [
+//     // TestTests,
+//     // SumTests
+    
+//     // TensorTests,
+//     // TensorGradTests,
+//     // NNTests,
+//     // NetworkTests,
+//     NetworksTests
+// ]
+
+// TestRunner.describe = async (name: string, func: Function) => {
+//     const colors = {"red": "\x1b[31m", "green": "\x1b[32m", "yellow": "\x1b[33m"};
+
+//     let passed = false;
+//     try {
+//         await func();
+
+//         console.log(`${colors["yellow"]}[${name}] ${colors["green"]} Passed`);
+
+//     } catch (error) {
+//         console.error("Error", error);
+//         passed = false;
+//         console.log(`${colors["yellow"]}[${name}] ${colors["red"]} Failed`);
+//         throw Error("Test failed")
+//     }
+// }
+
+// for (let unitTest of UnitTests) {
+//     new Promise(resolve => setTimeout(resolve, 0)).then(value => {
+//         console.log(`--------`);
+//         console.log(`Running test: ${unitTest.category}`);
+//         unitTest.func(Device.CPU);
+//     })
+// }
